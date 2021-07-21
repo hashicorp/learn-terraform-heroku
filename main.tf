dafcf3675ff46a0f229b32751cb24d39f7ae0aa1
Tu Nguyen <im2nguyen@gmail.com>
2021-07-21 08:09:10 -0700

    Add final configuration

--- main.tf
+++ main.tf
@@ -33,7 +33,12 @@ resource "heroku_build" "example" {
 resource "heroku_formation" "example" {
   app        = heroku_app.example.id
   type       = "web"
-  quantity   = 1
+  quantity   = 2
   size       = "Standard-1x"
   depends_on = [heroku_build.example]
+}
+
+resource "heroku_addon" "logging" {
+  app  = heroku_app.example.id
+  plan = "papertrail:choklad"
 }
