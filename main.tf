e6ba9506d11520bc3af32ab0abe8f5903562b52b
Tu Nguyen <im2nguyen@gmail.com>
2021-07-29 22:16:54 -0700

    Use app quantity variable for formation instead

--- main.tf
+++ main.tf
@@ -22,11 +22,16 @@ resource "heroku_build" "example" {
   }
 }
 
+variable "app_quantity" {
+  default     = 1
+  description = "Number of dynos in your Heroku formation"
+}
+
 # Launch the app's web process by scaling-up
 resource "heroku_formation" "example" {
   app        = heroku_app.example.id
   type       = "web"
-  quantity   = 2
+  quantity   = var.app_quantity
   size       = "Standard-1x"
   depends_on = [heroku_build.example]
 }
